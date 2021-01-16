cb08e8813448924a53a07391e3c9030cca301bae
Nadiia-Mykhalevych <nadiia.mykhalevych7@gmail.com>
2021-01-17 01:07:05 +0200

    added bar-chart

--- cursor-project/src/index.js
+++ cursor-project/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Chart from './charts/bar-chart/bar-chart'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Chart />
   </React.StrictMode>,
   document.getElementById('root')
 )
